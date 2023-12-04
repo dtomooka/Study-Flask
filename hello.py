00a5e94158f77108290502b51481db7339a44204
Devon Tomooka <devontomooka@gmail.com>
2023-12-04 12:58:07 -0800

    FF#5 Additions/Changes

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 # Create Flask instance
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "my super secret key"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Create route decorator
 @app.route("/")
@@ -14,13 +25,27 @@ def index():
                            stuff=stuff,
                            favorite_pizza=favorite_pizza)
 
+
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", user_name=name)
 
 
-# Create Custom Error Pages
+# Create Name Page
+@app.route("/name", methods=["GET", "POST"])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("name.html",
+                           name=name,
+                           form=form)
 
+
+# Create Custom Error Pages
 # Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
@@ -31,6 +56,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
 
 
 
